129f6b93ffe4fccaafccc9081230c66a23c4c25a
planckshema <shema.planck@eagles.oc.edu>
2025-11-20 16:40:40 -0600

    worlflow configuration

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/tracker-t8", routes);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3128;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
